cafb6b618db7df1d71141078c78c9e930cde5899
Matt Gardner <wilburnforce@gmail.com>
2016-07-15 16:12:44 -0400

    Moving risk category back to model.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,14 +1,30 @@
 class Forecast < ActiveRecord::Base
 
 	extend Enumerize 
-
-  include Forecast::RiskCategory
-
+  before_save :set_risk_category
 	belongs_to :report
 
 	validates :peak_load, presence: true
 
 	enumerize :risk, in: [:unlikely, :possible, :likely, :unknown], default: :unknown
+
+  def set_risk_category
+    # Always retrieve the latest configuration
+    config = Config.order("created_at ASC").last
+
+    min = config.possible_min
+    max = config.possible_max
+
+    likely = Range.new(min, max)
+
+    result = case
+       when likely.include?(peak_load) then "possible"
+       when peak_load < likely.min then "unlikely"
+       when peak_load > likely.min then "likely"
+    end
+
+    self.risk = result
+  end
 
   def self.to_csv
     CSV.generate do |csv|
