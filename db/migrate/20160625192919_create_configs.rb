e3b942a90cfb8549e53db4ff7860f998727167c6
Matt Cloyd <cloydster@gmail.com>
2016-07-20 12:12:37 -0400

    Redo table renaming
    Why:
    
    I did not realize the app was sort of already in production. I need to
    undo the change to the create_configs migration, and add a new migration
    to rename the table on Heroku.
    
    This change addresses the need by:
    
    * Changing create_configurations back to create_configs.
    
    * Adding a table-renaming migration.

--- db/migrate/20160625192919_create_configs.rb
+++ db/migrate/20160625192919_create_configs.rb
@@ -1,6 +1,6 @@
-class CreateConfigurations < ActiveRecord::Migration
+class CreateConfigs < ActiveRecord::Migration
   def change
-    create_table :configurations do |t|
+    create_table :configs do |t|
     	t.integer :possible_min
     	t.integer :possible_max
 
